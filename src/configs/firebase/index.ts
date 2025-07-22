cc6b8ac4e5714679f9a2e5048854910e70ba6450
Shivam Srivastava <percyj456@gmail.com>
2025-07-22 19:59:06 +0530

    Update index.ts

--- src/configs/firebase/index.ts
+++ src/configs/firebase/index.ts
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";// TODO: Add SDKs for Firebase prod
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDiyvPS5HbaaTzEr_d7HtEkrLbmFMYuPbQ",
+  apiKey: "YOUR_API_KEY",
   authDomain: "mike-14f7b.firebaseapp.com",
   projectId: "mike-14f7b",
   storageBucket: "mike-14f7b.appspot.com",
